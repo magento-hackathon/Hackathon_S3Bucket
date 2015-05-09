c5017fa31456a10faa008ab998351c32b3d0ba90
Andreas Emer <emer@mothership.de>
2015-05-09 11:37:48 +0200

    Init of the extension sceleton

---
+++ app/code/community/Hackathon/S3Bucket/Helper/Data.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * PHP Version 5.5
+ *
+ * @category  Hackathon
+ * @package   Hackathon_S3Bucket
+ * @author    Andreas Emer <emer AT mothership.de>
+ * @author    Alexander Turiak <turyak AT gmail.com>
+ * @copyright Copyright (c) 2015 Hackathon
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://mage-hackathon.de
+ */ 
+class Hackathon_S3Bucket_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
